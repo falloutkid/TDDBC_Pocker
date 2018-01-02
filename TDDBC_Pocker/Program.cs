9fb3dfa74437bf15486529062601a401f2c6d8d1
falloutkid <beck-13@mail.goo.ne.jp>
2018-01-03 06:35:01 +0900

    first commit

---
+++ TDDBC_Pocker/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDDBC_Pocker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ UnitTestProject/UnitTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TDDBC_Pocker;
+using Moq;
+
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest
+    {
+        public TestContext TestContext { set; get; }
+
+        [TestMethod]
+        [TestCase(1,2,3)]
+        public void SampleTestMethod()
+        {
+            TestContext.Run((int x, int y, int z) =>
+            {
+                // Assert.AreEqual(x + y, z);
+                (x + y).Is(z);
+            });
+        }
+    }
+}
