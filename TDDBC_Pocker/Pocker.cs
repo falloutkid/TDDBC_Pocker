d65aa8afacab0eae31dfd925df2182f4780c8c41
falloutkid <beck-13@mail.goo.ne.jp>
2018-01-03 21:59:09 +0900

    クラスの作成ごとに文字列変換の辞書型変数を持つことは無駄なので外部に抽出する

--- TDDBC_Pocker/Pocker.cs
+++ TDDBC_Pocker/Pocker.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace TDDBC_Pocker
 {
+    /// <summary>
+    /// トランプの数字一覧を列挙化
+    /// </summary>
     public enum RANK
     {
         Ace = 1,
@@ -23,6 +26,9 @@ namespace TDDBC_Pocker
         King
     }
 
+    /// <summary>
+    /// トランプのマークを列挙化
+    /// </summary>
     public enum SUIT
     {
         Spade,
@@ -31,41 +37,59 @@ namespace TDDBC_Pocker
         Diamond
     }
 
-    public class Card
+    /// <summary>
+    /// 文字列変換を行うクラスは全カード共通のため、1つのクラスとして抽出
+    /// プライベートなシングルトンとして用意する
+    /// </summary>
+    class CardNotation
     {
-        public RANK Rank { get; }
-        public SUIT Suit { get; }
+        public Dictionary<SUIT, string> str_suit_;
+        public Dictionary<RANK, string> str_rank_;
 
-        private Dictionary<SUIT, string> str_suit_;
-        private Dictionary<RANK, string> str_rank_;
+        public static CardNotation Signleton = new CardNotation();
 
-        public Card(SUIT suit, RANK rank)
+        public CardNotation()
         {
-            this.Suit = suit;
-            this.Rank = rank;
-
+            /// カードの数値と文字列の辞書型のメモリを作成
             str_rank_ = new Dictionary<RANK, string>();
             str_rank_.Add(RANK.Ace, "A");
             str_rank_.Add(RANK.King, "K");
             str_rank_.Add(RANK.Queen, "Q");
             str_rank_.Add(RANK.Jack, "J");
 
-            for(int i = 2;i <= 10; i++)
+            for (int i = 2; i <= 10; i++)
             {
                 str_rank_.Add((RANK)i, i.ToString());
             }
 
+            /// カードのマークと文字列の辞書型のメモリを作成
             str_suit_ = new Dictionary<SUIT, string>();
             str_suit_.Add(SUIT.Club, "♣");
             str_suit_.Add(SUIT.Heart, "♥");
             str_suit_.Add(SUIT.Diamond, "♦");
             str_suit_.Add(SUIT.Spade, "♠");
         }
+    }
 
-        
+    public class Card
+    {
+        public RANK Rank { get; }
+        public SUIT Suit { get; }
+
+        public Card(SUIT suit, RANK rank)
+        {
+            this.Suit = suit;
+            this.Rank = rank;    
+        }
+ 
+        /// <summary>
+        /// デフォルトの文字列変換を関数オーバーライドして使用する
+        /// 結果を作成する際は静的なシングルトンから作成する
+        /// </summary>
+        /// <returns></returns>
         public override string ToString()
         {
-            return str_rank_[this.Rank] + str_suit_[this.Suit];
+            return CardNotation.Signleton.str_rank_[this.Rank] + CardNotation.Signleton.str_suit_[this.Suit];
         }
     }
 
