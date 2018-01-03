757140303636f04e84897477c137ef8dacdbb237
falloutkid <beck-13@mail.goo.ne.jp>
2018-01-03 16:54:40 +0900

    前提条件をEnum値として定義する

--- TDDBC_Pocker/Pocker.cs
+++ TDDBC_Pocker/Pocker.cs
@@ -6,6 +6,31 @@ using System.Threading.Tasks;
 
 namespace TDDBC_Pocker
 {
+    public enum Rank
+    {
+        Ace = 1,
+        Two,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Ten,
+        Jack,
+        Queen,
+        King
+    }
+
+    public enum Suit
+    {
+        Spade,
+        Heart,
+        Club,
+        Diamond
+    }
+
     public class Pocker
     {
     }
